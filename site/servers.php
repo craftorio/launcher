9a20475285fc9a6e2b34cc3cda344091d7fe69f4
zenit <zenit@m.ru>
2015-05-01 07:57:08 +0300

    .

--- site/servers.php
+++ site/servers.php
@@ -6,7 +6,6 @@
 echo '
 v152, localhost, 25565, 1.5.2<::>
 voxelaria164, 188.165.137.202, 25675, 1.6.4<::>
-voxelaria172, localhost, 25565, 1.7.2<::>
 voxelaria1710, localhost, 25565, 1.7.10<::>
 vanilla181, localhost, 25565, 1.8.1<::>
 ';
