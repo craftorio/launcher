ef163650ae718fcc374afebb9f021da65bd21591
zenit <zenit@m.ru>
2015-05-04 03:35:28 +0300

    .

--- site/uuidskull.php
+++
@@ -1,5 +0,0 @@
-<?php
-include_once("uuid.php");
-foreach($_REQUEST as $key => $val)
-$user = str_replace(array('[', ']'), array('', ''), $key);
-echo '{"id":"'.uuidConvert($user).'","name":'.$user.'}';
