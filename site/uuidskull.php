47f5651e7f536ed5d84697ddcb77d166352505ee
zenit <zenit@m.ru>
2015-06-03 17:39:29 +0300

    fix

--- site/uuidskull.php
+++ site/uuidskull.php
@@ -3,7 +3,7 @@ define('INCLUDE_CHECK',true);
 include_once("connect.php");
 include_once("uuid.php");
 @$user = json_decode($HTTP_RAW_POST_DATA);
-   try { 
+try { 
     $stmt = $db->prepare("SELECT user FROM usersession WHERE user= :user");
     $stmt->bindValue(':user', $user[0]);
     $stmt->execute();
@@ -12,7 +12,7 @@ include_once("uuid.php");
     if($realUser==null) {
         exit;
     }
-	echo '[{"id":"'.str_replace('-', '', @uuidConvert($realUser)).'","name":"'.$realUser.'"}]';
+    echo '[{"id":"'.str_replace('-', '', @uuidConvert($realUser)).'","name":"'.$realUser.'"}]';
 } catch(PDOException $pe) {
-	die($logger->WriteLine($log_date.$pe));  //вывод ошибок MySQL в m.log
+    die($logger->WriteLine($log_date.$pe));  //вывод ошибок MySQL в m.log
 }
