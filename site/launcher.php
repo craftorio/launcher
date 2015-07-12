685b9f59ee658598f4ea771cd40e4e8ba3c315bc
zenit <zenit@m.ru>
2015-07-12 21:46:33 +0300

    .

--- site/launcher.php
+++ site/launcher.php
@@ -9,7 +9,7 @@
 		$x  = $_POST['action'];
 		$x = str_replace(" ", "+", $x);
 		$yd = Security::decrypt($x, $key2);
-		list($action, $client, $login, $postPass, $launchermd5, $ctoken) = explode(':', $yd);
+		@list($action, $client, $login, $postPass, $launchermd5, $ctoken) = explode(':', $yd);
 	} else {
 		exit;
 	}
