79a4a0333ec9c25a5bb769e326b6a2ad8a87cb99
SergeyCherepanov <sergey@cherepanov.org.ua>
2015-12-30 21:18:11 +0400

    update

--- src/main/java/net/launcher/run/Launcher.java
+++ src/main/java/net/launcher/run/Launcher.java
@@ -1,46 +1,42 @@
 package net.launcher.run;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import net.launcher.MusPlay;
 import net.launcher.components.Frame;
 import net.launcher.utils.BaseUtils;
 
-public class Launcher
-{
-	public static void main(String[] args) throws Exception
-	{
-		boolean isRenderUi = (args.length != 0)?(args[0].equals("true")?true:false):false;
+import java.io.*;
+
+public class Launcher {
+    public static void main(String[] args) throws Exception {
+        boolean isRenderUi = (args.length != 0) ? (args[0].equals("true") ? true : false) : false;
         if (isRenderUi) {
-			File dir = new File(BaseUtils.getAssetsDir().toString());
-			if(!dir.exists()) dir.mkdirs();
-			InputStream stream 		  = Starter.class.getResourceAsStream("/assets/textures/gui/favicon.png");
-			OutputStream resStreamOut = null;
-			int readBytes;
-		    byte[] buffer = new byte[4096];
-		    try {
-		        resStreamOut = new FileOutputStream(new File(BaseUtils.getAssetsDir().toString()+"/favicon.png"));
-		        while ((readBytes = stream.read(buffer)) > 0) {
-		            resStreamOut.write(buffer, 0, readBytes);
-		        }
-		    } catch (IOException e1) {
-		        e1.printStackTrace();
-		    } finally {
-		        stream.close();
-		        resStreamOut.close();
-		    }
-			Frame.start();
-			if (BaseUtils.getPropertyBoolean("Music", true)) {
-	           new MusPlay(Settings.iMusicname);
-	        }
-        } else {
+            File dir = new File(BaseUtils.getAssetsDir().toString());
+            if (!dir.exists())
+                dir.mkdirs();
+            InputStream stream = Starter.class.getResourceAsStream("/assets/textures/gui/favicon.png");
+            OutputStream resStreamOut = null;
+            int readBytes;
+            byte[] buffer = new byte[4096];
+            try {
+                resStreamOut = new FileOutputStream(new File(BaseUtils.getAssetsDir().toString() + "/favicon.png"));
+                while ((readBytes = stream.read(buffer)) > 0) {
+                    resStreamOut.write(buffer, 0, readBytes);
+                }
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            } finally {
+                stream.close();
+                resStreamOut.close();
+            }
             Frame.start();
-            //Starter.main(null);
+            if (BaseUtils.getPropertyBoolean("Music", true)) {
+                new MusPlay(Settings.iMusicname);
+            }
+        } else {
+            //Frame.start();
+            Starter.main(null);
         }
-	}
+    }
 }
 
 
